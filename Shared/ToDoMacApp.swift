554ce4859ded78080b7123dc6f87052dcb539afb
Milkyo <dp221125@naver.com>
2021-02-12 16:57:08 +0900

    Initial Commit

---
+++ Shared/ToDoMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMacApp.swift
+//  Shared
+//
+//  Created by Seokho on 2021/02/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
